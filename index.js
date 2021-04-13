0989f38970c6697ea08c00dd3d0c402b0f23eade
Matheus Sousa <matheusoficial.sousa@gmail.com>
2021-04-13 11:04:38 -0300

    creating connection with database

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('tesaaste')
+const { Client } = require('pg')
+
+const client = new Client({
+    user: "matheus",
+    password: "root",
+    host: "localhost",
+    port: 8080,
+    database: "fabric"
+})
+
+
+async function execute() {
+    try {
+      await client.connect();
+      console.log("Connected succesfully");
+    } 
+    
+    catch (e) {
+      console.log('Something went wrong');
+    } 
+    
+    finally {
+      await client.end();
+      console.log("Client disconnected successfully");
+    }
+  }
+  
+  execute();
